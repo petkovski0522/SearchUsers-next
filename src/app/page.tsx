aa3b68d339134d98e35a3f0da3cace8ee9fc2614
Kristijan Petkovski <petkovski0522@gmail.com>
2024-01-14 22:48:40 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full bg-stone-100  ">
-      <div className=""></div>
+    <div className="flex min-h-screen w-full bg-stone-100 p-1.5 sm:p-4 pt-10 sm:pt-12 transition-all  dark:bg-slate-900 ">
+      <div className="  mx-auto flex w-full max-w-[600px] flex-col gap-8  rounded p-2">
+        <section className="flex justify-between gap-3">
+          <p className="text-xl font-semibold">DevExplorer</p>
+          <p>Light</p>
+        </section>
+      </div>
     </div>
   );
 }
