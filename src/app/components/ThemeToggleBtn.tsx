1266a2a31d0bfe27161d56a2458aca34de3cb458
Kristijan Petkovski <petkovski0522@gmail.com>
2024-01-15 11:31:37 +0100

    commit

--- src/app/components/ThemeToggleBtn.tsx
+++ src/app/components/ThemeToggleBtn.tsx
@@ -1,14 +1,28 @@
+/** @format */
+"use client";
 import React from "react";
 import { IoSunnyOutline } from "react-icons/io5";
 import { LuMoonStar } from "react-icons/lu";
-
+import { useTheme } from "next-themes";
 type Props = {};
 
-export default function ThemeToggleBtn({}: Props) {
+export default function DarkAndLightBtn({}: Props) {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  function toggleDark() {
+    if (resolvedTheme === "light") setTheme("dark");
+    if (resolvedTheme === "dark") setTheme("light");
+  }
+
   return (
-    <div className="flex items-center gap-2">
-      <p className="team-sm">Light</p>
-      <LuMoonStar /> <IoSunnyOutline />
+    <div className="flex items-center gap-2" onClick={toggleDark}>
+      <p className="text-sm cursor-pointer">
+        {resolvedTheme === "light" ? "DARK" : "LIGHT"}
+      </p>
+
+      <button className="text-2xl">
+        {resolvedTheme === "light" ? <LuMoonStar /> : <IoSunnyOutline />}
+      </button>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ThemeToggleBtn from "./components/ThemeToggleBtn";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full bg-stone-100 p-1.5 sm:p-4 pt-10 sm:pt-12 transition-all  dark:bg-slate-900 ">
+    <div className="flex min-h-screen w-full  p-1.5 sm:p-4 pt-10 sm:pt-12 transition-all  dark:bg-slate-900 ">
       <div className="  mx-auto flex w-full max-w-[600px] flex-col gap-8  rounded p-2">
         <section className="flex justify-between gap-3">
           <p className="text-xl font-semibold">DevExplorer</p>
