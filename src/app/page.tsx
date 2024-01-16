f6d9727b9a26aadcd0389ead04338bcdf0d7cca1
Kristijan Petkovski <petkovski0522@gmail.com>
2024-01-16 17:18:21 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen w-full  p-1.5 sm:p-4 pt-10 sm:pt-12 transition-all  dark:bg-slate-900 ">
       <div className="  mx-auto flex w-full max-w-[600px] flex-col gap-8  rounded p-2">
-        <section className="flex justify-between gap-3">
+        <section className="flex justify-between gap-3 border-2 border-solid  border-dark:bg-slate-900">
           <p className="text-xl font-semibold">DevExplorer</p>
 
           <ThemeToggleBtn />
